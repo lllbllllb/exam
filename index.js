6f16a23e368289042dbb8429c82883640f2ce1d5
mbllllb <enakes@gmail.com>
2018-09-10 02:25:52 +0300

    fourth

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// https://stackoverflow.com/questions/31495743/node-angular-app-uncaught-syntaxerror-unexpected-token
-
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -16,20 +14,3 @@ app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running...'));
 
-/*
-const express = require('express');
-const path = require('path');
-const http = require('http');
-
-const app = express();
-
-const port = process.env.PORT || '5000';
-
-app.use(express.static(__dirname, '/dist/c'));
-
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log('Running...'));
-*/
