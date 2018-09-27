f2e8b755ad5e2a0424706c29547cd3fe7d2a947b
mbllllb <enakes@gmail.com>
2018-09-28 01:38:51 +0300

    pre-finish 2.0

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const http = require('http');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/exam')));
+app.use(requireHTTPS);
 
 app.all('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/exam/index.html')));
 
@@ -14,3 +15,10 @@ app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log('Running...'));
 
+function requireHTTPS(req, res, next) {
+  if (!req.secure) {
+    //FYI this should work for local development as well
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
